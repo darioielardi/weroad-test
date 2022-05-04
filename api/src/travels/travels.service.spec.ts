20a19477387b79424c7dabc6b1219a99e87e579a
Dario Ielardi <dario.ielardi@gmail.com>
2022-05-04 15:28:41 +0200

    create travel test

--- api/src/travels/travels.service.spec.ts
+++ api/src/travels/travels.service.spec.ts
@@ -1,18 +1,63 @@
+import { getRepositoryToken } from '@mikro-orm/nestjs';
+import { ConflictException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Travel } from './entities/travel.entity';
 import { TravelsService } from './travels.service';
+
+const mockedRepo = {
+  findOne: jest.fn(),
+  create: jest.fn(),
+  persistAndFlush: jest.fn(),
+};
 
 describe('TravelsService', () => {
   let service: TravelsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TravelsService],
+      imports: [],
+      providers: [
+        TravelsService,
+        { provide: getRepositoryToken(Travel), useValue: mockedRepo },
+      ],
     }).compile();
 
     service = module.get<TravelsService>(TravelsService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('create travel', () => {
+    const alreadyExistingSlug = 'existing-slug';
+
+    beforeEach(() => {
+      mockedRepo.findOne.mockImplementationOnce((params: { slug: string }) => {
+        if (params.slug === alreadyExistingSlug) {
+          return new Travel();
+        }
+      });
+
+      mockedRepo.create.mockReturnValueOnce(new Travel());
+    });
+
+    test('existing by slug', async () => {
+      await expect(
+        service.create({
+          name: 'test',
+          slug: alreadyExistingSlug,
+          description: 'test',
+          numberOfDays: 1,
+        }),
+      ).rejects.toThrow(ConflictException);
+    });
+
+    test('new travel', async () => {
+      await expect(
+        service.create({
+          name: 'test',
+          slug: 'test',
+          description: 'test',
+          numberOfDays: 1,
+        }),
+      ).resolves.toBeInstanceOf(Travel);
+    });
   });
 });
